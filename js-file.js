27be9b8c8af6b8268f52bc26dea871028936b3d6
Ryan <ryanying1102@gmail.com>
2024-05-27 20:58:32 -0400

    rps with UI

--- js-file.js
+++ js-file.js
@@ -1,25 +1,71 @@
-// your JavaScript file
-function alertFunction () {
-    alert ('yay!');
+function getComputerChoice () {
+    v = Math.floor(Math.random() * 3);
+    return v;
 }
 
-const container = document.querySelector("#container");
+// function getHumanChoice () {
+//     console.log(choice);
+//     return choice;
+// }
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+function playRound (humanChoice){
+    let computerChoice = getComputerChoice();
+    if(humanChoice == computerChoice){
+        msg.textContent = `Computer Chose ` + computerChoice + ` , Tie!`;
+    }
+    if(humanChoice == 0 && computerChoice == 1){
+        computerScore += 1;
+        compScore.textContent = computerScore;
+        msg.textContent = 'You Lose!'
+    }
+    if(humanChoice == 0 && computerChoice == 2){
+        humanScore += 1;
+        humScore.textContent = humanScore;
+        msg.textContent = 'You Win!'
+    }
+    if(humanChoice == 1 && computerChoice == 2){
+        computerScore += 1;
+        compScore.textContent = computerScore;
+        msg.textContent = 'You Lose!'
+    }
+    if(humanChoice == 1 && computerChoice == 0){
+        humanScore += 1;
+        humScore.textContent = humanScore;
+        msg.textContent = 'You Win!'
+    }
+    if(humanChoice == 2 && computerChoice == 0){
+        computerScore += 1;
+        compScore.textContent = computerScore;
+        msg.textContent = 'You Lose!'
+    }
 
-container.appendChild(content);
 
-const para = document.createElement('p');
-para.setAttribute('style','color: red');
-para.textContent = 'red text!';
+}
 
-container.appendChild(para);
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', function(e){
-    console.log(e);
-    e.target.style.background = 'blue';
+let humanScore = 0;
+let computerScore = 0;
+
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+const humScore = document.querySelector('.humanScore');
+const compScore = document.querySelector('.computerScore');
+const msg = document.querySelector('.result');
+
+humScore.textContent = humanScore;
+compScore.textContent = computerScore;
+
+rock.addEventListener('click', () => {
+    playRound(0);
 });
 
+paper.addEventListener('click', () => {
+    playRound(1);
+});
+
+scissors.addEventListener('click', () => {
+    playRound(2);
+});
